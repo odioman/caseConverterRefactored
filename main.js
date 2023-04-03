5617a14b4fcc0c0bcdaae06f0093a2b88534ce83
Thomas Koshy <ttkoshy@gmail.com>
2023-04-03 10:35:51 -0400

    check the tech

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 const snakeCase = ['snake_case', 'another_example', 'can_be_many_words'];
 
 function snakeCaseToPascalCase(snakeCase) {
-  const joinWordArr = [];
+ const finalArr = [];
  const snakeCaseSplit = snakeCase.map(string => string.split('_'));
   console.log(snakeCaseSplit)
   for (let i = 0; i < snakeCaseSplit.length; i++) {
+    const joinWordArr = [];
      for (let j = 0; j < snakeCaseSplit[i].length; j++) {
        const capitalLetter = snakeCaseSplit[i][j][0].toUpperCase();
        console.log(capitalLetter)
@@ -12,11 +13,12 @@ function snakeCaseToPascalCase(snakeCase) {
        console.log(restOfWord);
        const joinWord = capitalLetter + restOfWord;
        console.log(joinWord);
-       return joinWord
+       joinWordArr.push(joinWord) 
      }
-    joinWordArr.push(joinWord)
-    console.log(joinWordArr)
- 
+       const pascalCase = joinWordArr.join('');
+       console.log('pascalCase: ', pascalCase)
+       finalArr.push(pascalCase);
+       console.log("finalArr: ", finalArr); 
     }
-  
-  }
+    return finalArr
+}
