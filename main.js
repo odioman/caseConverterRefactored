fd8b649d417f374ee2f0406ed6d5d7efdd93b1a2
Thomas Koshy <ttkoshy@gmail.com>
2023-04-03 18:48:09 -0400

    check the tech with .map()

--- main.js
+++ main.js
@@ -22,3 +22,31 @@ function snakeCaseToPascalCase(snakeCase) {
     }
     return finalArr
 }
+
+function snakeCaseToCamelCaseConverter(snakeCase) {
+    const finalArr = [];
+    const snakeCaseSplit = snakeCase.map(string => string.split('_'))
+    console.log(snakeCaseSplit)
+    for (let i = 0; i <snakeCaseSplit.length; i++) {
+      const joinWordArr = [];
+      for (let j=1; j<snakeCaseSplit[i].length; j++) {
+        const capitalLetter = snakeCaseSplit[i][j][0].toUpperCase();
+        console.log(capitalLetter);
+        const restOfWord = snakeCaseSplit[i][j].slice(1);
+        console.log(restOfWord);
+        const joinWord = capitalLetter + restOfWord
+        console.log(joinWord);
+        joinWordArr.push(joinWord)
+        console.log(joinWordArr)
+      }
+      joinWordArr.unshift(snakeCaseSplit[i][0])
+      console.log(joinWordArr)
+      const camelCase = joinWordArr.join('');
+      console.log("camelCase: ", camelCase);
+      finalArr.push(camelCase);
+      console.log(finalArr)
+    }
+    return finalArr;
+  }
+  
+  console.log(snakeCaseToCamelCaseConverter(snakeCase));
