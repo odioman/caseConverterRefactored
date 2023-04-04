14a4f78800397c7181c05677f7b55aa53047d1a4
Thomas Koshy <ttkoshy@gmail.com>
2023-04-04 18:56:58 -0400

    check the tech using .map()

--- main.js
+++ main.js
@@ -24,29 +24,42 @@ function snakeCaseToPascalCase(snakeCase) {
 }
 
 function snakeCaseToCamelCaseConverter(snakeCase) {
-    const finalArr = [];
-    const snakeCaseSplit = snakeCase.map(string => string.split('_'))
-    console.log(snakeCaseSplit)
-    for (let i = 0; i <snakeCaseSplit.length; i++) {
-      const joinWordArr = [];
-      for (let j=1; j<snakeCaseSplit[i].length; j++) {
-        const capitalLetter = snakeCaseSplit[i][j][0].toUpperCase();
-        console.log(capitalLetter);
-        const restOfWord = snakeCaseSplit[i][j].slice(1);
-        console.log(restOfWord);
-        const joinWord = capitalLetter + restOfWord
-        console.log(joinWord);
-        joinWordArr.push(joinWord)
-        console.log(joinWordArr)
-      }
-      joinWordArr.unshift(snakeCaseSplit[i][0])
+  const finalArr = [];
+  const snakeCaseSplit = snakeCase.map(string => string.split('_'))
+  console.log(snakeCaseSplit)
+  for (let i = 0; i <snakeCaseSplit.length; i++) {
+    const joinWordArr = [];
+    for (let j=1; j<snakeCaseSplit[i].length; j++) {
+      const capitalLetter = snakeCaseSplit[i][j][0].toUpperCase();
+      console.log(capitalLetter);
+      const restOfWord = snakeCaseSplit[i][j].slice(1);
+      console.log(restOfWord);
+      const joinWord = capitalLetter + restOfWord
+      console.log(joinWord);
+      joinWordArr.push(joinWord)
       console.log(joinWordArr)
-      const camelCase = joinWordArr.join('');
-      console.log("camelCase: ", camelCase);
-      finalArr.push(camelCase);
-      console.log(finalArr)
     }
-    return finalArr;
+    joinWordArr.unshift(snakeCaseSplit[i][0])
+    console.log(joinWordArr)
+    const camelCase = joinWordArr.join('');
+    console.log("camelCase: ", camelCase);
+    finalArr.push(camelCase);
+    console.log(finalArr)
   }
-  
-  console.log(snakeCaseToCamelCaseConverter(snakeCase));
+  return finalArr;
+}
+
+//console.log(snakeCaseToCamelCaseConverter(snakeCase));
+
+const kebabCaseObj = { key1: 'value-1', key2: 'value-two'}
+
+function kebabCaseToSnakeCaseConverter(kebabCaseObj) {
+  const strSplit = Object.values(kebabCaseObj).map(string => string.split('-'));
+  const strJoin = strSplit.map(string => string.join('_'));
+  kebabCaseObj['key1'] = strJoin[0];
+  kebabCaseObj['key2'] = strJoin[1];
+  //console.log(kebabCaseObj);
+  return kebabCaseObj
+}
+
+console.log(kebabCaseToSnakeCaseConverter(kebabCaseObj));
